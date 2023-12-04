f8cb1fef6d16cca998439b3d25f00966d1f173e8
Prasad Bhosle <Prasad.Bhosle@teknopoint.local>
2023-12-05 01:21:43 +0530

    portfolio done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
 
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 justify-between`}>
+      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-12`}>
         <div className='flex flex-col justify-center items-center mt-7 mr-10'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -34,7 +34,7 @@ const Hero = () => {
           <DownloadButton />
         </div>
 
-        <div className={`profilePos object-cover object-top hidden sm:block absolute -z-[1]`}>
+        <div className={`profilePos object-cover object-top hidden sm:block  absolute -z-[1]`}>
           <img className='' src={profileImage.img} alt="" />
 
         </div>
